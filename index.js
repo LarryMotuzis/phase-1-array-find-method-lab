983de3114222a43782648cbce8d56a497e0713ee
Larry Motuzis <larrymotuzis@gmail.com>
2022-01-14 17:08:42 -0600

    commit tree 8f8ba7686f13eda2612db777ba60bed07a2531da

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "L"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+
+
+
+function superbowlWin(record){
+    let winYear = record.find(record => record.result === "W");
+    if (winYear){
+        return winYear.year
+        
+    }   else {return undefined}
+}
+console.log(superbowlWin(record))
